ab67c39faa2bab20483dcc3d2a89372cf562e07f
Ahmed Turkmani <ahmedturkmani@192.168.1.77>
2016-11-18 20:33:50 -0700

    Meged backup app and server app they are now 1. if server is started and no primary is found it will start as primary else as backup.

---
+++ src/ClientMain.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import asciiPanel.AsciiPanel;
+import debug.Logger;
+import game.GameController;
+import game.GameRenderer;
+import game.GameState;
+import networking.Client;
+import networking.Server;
+
+import java.awt.*;
+
+
+public class ClientMain {
+
+	private static void SetupClient()
+    {
+    	
+        Thread thread;
+
+        // Create the renderer and run on thread
+        GameRenderer renderer = new GameRenderer();
+        thread = new Thread(renderer, "Renderer");
+        thread.start();
+
+        // Create client and run on thread
+        Client client = new Client();
+        thread = new Thread(client, "Client");
+        thread.start();
+
+        // Create the controller and add it to the JFrame
+        GameController controller = new GameController(client);
+        renderer.addController(controller);
+    }
+	public static void main(String[] args) {
+		   // Create a new game state
+		Logger.debug=true;
+        new GameState();
+		SetupClient();
+
+	}
+
+}
